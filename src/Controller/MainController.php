0956ec4bf6c5b35f9354b3e37e751def17877111
MargauxPuget <margaux.puget@gmail.com>
2023-02-27 14:32:01 +0100

    ajout des autheurs sur la page d'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +14,20 @@ class MainController extends AbstractController
      * @Route("/", name="default" )
      * @Route("/main", name="app_main")
      */
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, AuthorRepository $authorRepository): Response
     {
         // TODO :  aller cherche tous les posts de la BDD
         $allPostfromBDD = $postRepository->findAll();
-        dump($allPostfromBDD);
+        // dump($allPostfromBDD);
+
+        // TODO :  aller cherche tous les autheurs de la BDD
+        $allAuthorfromBDD = $authorRepository->findAll();
+        
+
         return $this->render('main/index.html.twig', [
             'pageName' => 'Accueil',
-            'allPosts' => $allPostfromBDD
+            'allPosts' => $allPostfromBDD,
+            'allAuthors' => $allAuthorfromBDD
         ]);
     }
 
